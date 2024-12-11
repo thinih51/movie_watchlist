9f9cb342c83d5fddda06ccbb64bb175ce55e19a4
thinih51 <shedathi@students.zhaw.ch>
2024-12-11 14:19:57 +0100

    change db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
-import { MongoClient, ObjectId } from "mongodb"; // See https://www.mongodb.com/docs/drivers/node/current/quick-start/
+import { MongoClient, ObjectId } from "mongodb";
 import { DB_URI } from "$env/static/private";
 
 const client = new MongoClient(DB_URI);
@@ -16,18 +16,13 @@ async function getMovies() {
   try {
     const collection = db.collection("movies");
 
-    // You can specify a query/filter here
-    // See https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/query-document/
     const query = {};
-
-    // Get all objects that match the query
     movies = await collection.find(query).toArray();
     movies.forEach((movie) => {
-      movie._id = movie._id.toString(); // convert ObjectId to String
+      movie._id = movie._id.toString();
     });
   } catch (error) {
     console.log(error);
-    // TODO: errorhandling
   }
   return movies;
 }
@@ -37,34 +32,18 @@ async function getMovie(id) {
   let movie = null;
   try {
     const collection = db.collection("movies");
-    const query = { _id: new ObjectId(id) }; // filter by id
+    const query = { _id: new ObjectId(id) };
     movie = await collection.findOne(query);
 
     if (!movie) {
       console.log("No movie with id " + id);
-      // TODO: errorhandling
     } else {
-      movie._id = movie._id.toString(); // convert ObjectId to String
+      movie._id = movie._id.toString();
     }
   } catch (error) {
-    // TODO: errorhandling
     console.log(error.message);
   }
   return movie;
-}
-
-async function createMovie(movie) {
-  movie.poster = "/images/placeholder.jpg"; // Platzhalterbild, falls keine URL angegeben ist
-  movie.actors = []; // Leere Liste für Schauspieler
-  movie.watchlist = false; // Standardwert
-  try {
-      const collection = db.collection("movies");
-      const result = await collection.insertOne(movie);
-      return result.insertedId.toString(); // Rückgabe der Movie-ID
-  } catch (error) {
-      console.log(error.message);
-  }
-  return null;
 }
 
 async function createMovie(movie) {
@@ -73,61 +52,41 @@ async function createMovie(movie) {
   movie.watchlist = false;
 
   try {
-      const collection = db.collection("movies");
-      const result = await collection.insertOne(movie);
-      return result.insertedId.toString(); // ObjectId als String zurückgeben
-  } catch (error) {
-      console.log(error.message);
-  }
-  return null;
-}
-
-// update movie
-// Example movie object:
-/* 
-{ 
-  _id: "6630e72c95e12055f661ff13",
-  title: "Das Geheimnis von Altura",
-  year: 2024,
-  length: "120 Minuten",
-  actors: [
-    "Lena Herzog",
-    "Maximilian Schröder",
-    "Sophia Neumann"
-  ],
-  poster: "/images/Altura.png",
-  watchlist: false
-} 
-*/
-// returns: id of the updated movie or null, if movie could not be updated
-async function updateMovie(movie) {
-  try {
-    let id = movie._id;
-    delete movie._id; // delete the _id from the object, because the _id cannot be updated
     const collection = db.collection("movies");
-    const query = { _id: new ObjectId(id) }; // filter by id
-    const result = await collection.updateOne(query, { $set: movie });
-
-    if (result.matchedCount === 0) {
-      console.log("No movie with id " + id);
-      // TODO: errorhandling
-    } else {
-      console.log("Movie with id " + id + " has been updated.");
-      return id;
-    }
+    const result = await collection.insertOne(movie);
+    return result.insertedId.toString(); // ObjectId als String zurückgeben
   } catch (error) {
-    // TODO: errorhandling
     console.log(error.message);
   }
   return null;
 }
 
-// delete movie by id
-// returns: id of the deleted movie or null, if movie could not be deleted
+// Update movie
+async function updateMovie(movie) {
+  try {
+    let id = movie._id;
+    delete movie._id; // delete the _id from the object, because the _id cannot be updated
+    const collection = db.collection("movies");
+    const query = { _id: new ObjectId(id) };
+    const result = await collection.updateOne(query, { $set: movie });
+
+    if (result.matchedCount === 0) {
+      console.log("No movie with id " + id);
+    } else {
+      console.log("Movie with id " + id + " has been updated.");
+      return id;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+  return null;
+}
+
+// Delete movie by id
 async function deleteMovie(id) {
   try {
     const collection = db.collection("movies");
-    const query = { _id: new ObjectId(id) }; // filter by id
+    const query = { _id: new ObjectId(id) };
     const result = await collection.deleteOne(query);
 
     if (result.deletedCount === 0) {
@@ -137,13 +96,12 @@ async function deleteMovie(id) {
       return id;
     }
   } catch (error) {
-    // TODO: errorhandling
     console.log(error.message);
   }
   return null;
 }
 
-// export all functions so that they can be used in other files
+// Export all functions
 export default {
   getMovies,
   getMovie,
